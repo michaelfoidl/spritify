424aae81940c084d77ba94e6f24f11bb0957a4ba
Michael Foidl <se18m001@technikum-wien.at>
2021-07-05 11:46:44 +0200

    added solution with projects for api and web interface

---
+++ api/Spritify/Controllers/EchoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Spritify.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class EchoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Index(string message)
+        {
+            return Ok("Echo: " + message);
+        }
+    }
+}
---
+++ web/Spritify.Web/Controllers/UiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Spritify.Web.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class UiController : ControllerBase
+    {
+        [HttpGet("{*url:nonfile}")]
+        public IActionResult Index()
+        {
+            return File("~index.html", "text/html");
+        }
+    }
+}
