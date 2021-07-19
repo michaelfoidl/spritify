69a22295e788640c9fbc5562b7767de301cd475d
Michael Foidl <se18m001@technikum-wien.at>
2021-07-19 11:00:59 +0200

    renamed Spritify API project to Spritify.Api

--- api/Spritify.Api/Controllers/EchoController.cs
+++ api/Spritify.Api/Controllers/EchoController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Spritify.Controllers
+namespace Spritify.Api.Controllers
 {
     [ApiController]
     [Route("/")]
--- api/Spritify.Api/Startup.cs
+++ api/Spritify.Api/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Spritify
+namespace Spritify.Api
 {
     public class Startup
     {
