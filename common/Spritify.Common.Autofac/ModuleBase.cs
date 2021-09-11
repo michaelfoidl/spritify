michaelfoidl/spritify

commit 5bc70faaca5a4e8f216f6c6333a1b622e9ef527d

integrated/adapted a testFramework built on top of NUnit that was developed as part of another project, updated development build pipeline

.github/workflows/development.yml
@@ -21,6 +21,24 @@ jobs:
           nuget-cache: ${{ env.nuget-cache }}
           nuget-cache-version: ${{ env.nuget-cache-version }}
           artifact-name: spritify-common-build-${{ github.sha }}
+  build-testFramework:
+    runs-on: ubuntu-latest
+    needs: build-common
+    env:
+      dotnet-version: '5.0.x'
+      nuget-cache: ~/.nuget/packages/development/testFramework
+      nuget-cache-version: 1
+    steps:
+      - name: Checkout latest version
+        uses: actions/checkout@v2
+      - name: Build Spritify.TestFramework
+        uses: ./.github/actions/build-dotnet
+        with:
+          dotnet-version: ${{ env.dotnet-version }}
+          root-directory: ./testFramework
+          nuget-cache: ${{ env.nuget-cache }}
+          nuget-cache-version: ${{ env.nuget-cache-version }}
+          artifact-name: spritify-testFramework-build-${{ github.sha }}
   build-api:
     runs-on: ubuntu-latest
     needs: build-common

testFramework/Spritify.TestFramework.Extensions.Jwt/Spritify.TestFramework.Extensions.Jwt.csproj
@@ -1,0 +1,18 @@
+﻿<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <TargetFramework>net5.0</TargetFramework>
+    <RestorePackagesWithLockFile>true</RestorePackagesWithLockFile>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include="System.IdentityModel.Tokens.Jwt" Version="6.12.0" />
+  </ItemGroup>
+
+  <ItemGroup>
+    <ProjectReference Include="..\..\common\Spritify.Autofac\Spritify.Common.Autofac.csproj" />
+    <ProjectReference Include="..\..\common\Spritify.Common\Spritify.Common.csproj" />
+    <ProjectReference Include="..\Spritify.TestFramework\Spritify.TestFramework.csproj" />
+  </ItemGroup>
+
+</Project>

commit 2c85e18e384202aeb114b94840593cd712e43407

added Spritify.Common.Autofac, fixed build errors introduced in 5bc70fa, fixed build pipeline errors introduced in 5bc70fa

.github/workflows/development.yml
@@ -31,6 +31,10 @@ jobs:
     steps:
       - name: Checkout latest version
         uses: actions/checkout@v2
+      - name: Restore Spritify.Common build artifacts
+        uses: actions/download-artifact@v2
+        with:
+          name: spritify-common-build-${{ github.sha }}
       - name: Build Spritify.TestFramework
         uses: ./.github/actions/build-dotnet
         with:

Spritify.sln
@@ -7,6 +7,8 @@ Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "common", "common", "{6987E0
 EndProject
 Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Spritify.Common", "common\Spritify.Common\Spritify.Common.csproj", "{B3725C4E-7770-4467-BC74-2FCB8E4FC3B7}"
 EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Spritify.Common.Autofac", "common\Spritify.Common.Autofac\Spritify.Common.Autofac.csproj", "{3F83F56C-547D-4608-90A3-CD6FC1DD8D82}"
+EndProject
 Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "api", "api", "{BEDB5A79-A1F3-411B-9DE0-BC7FB1AD135D}"
 EndProject
 Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Spritify.Api", "api\Spritify.Api\Spritify.Api.csproj", "{C27F406E-2C02-4077-AF00-0F66F59806AF}"
@@ -114,6 +116,10 @@ Global
 		{1CC6029B-8D22-45EA-B181-3EA765F92CE4}.Debug|Any CPU.Build.0 = Debug|Any CPU
 		{1CC6029B-8D22-45EA-B181-3EA765F92CE4}.Release|Any CPU.ActiveCfg = Release|Any CPU
 		{1CC6029B-8D22-45EA-B181-3EA765F92CE4}.Release|Any CPU.Build.0 = Release|Any CPU
+		{3F83F56C-547D-4608-90A3-CD6FC1DD8D82}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{3F83F56C-547D-4608-90A3-CD6FC1DD8D82}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{3F83F56C-547D-4608-90A3-CD6FC1DD8D82}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{3F83F56C-547D-4608-90A3-CD6FC1DD8D82}.Release|Any CPU.Build.0 = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
@@ -136,6 +142,7 @@ Global
 		{68F1CD12-E0E8-4CD7-A883-C271601B08E2} = {7F194102-A8E6-4177-8FFA-97A46D2B5991}
 		{DBF7B8AC-848A-4D5F-A97E-DDFFBB9FCF78} = {7F194102-A8E6-4177-8FFA-97A46D2B5991}
 		{1CC6029B-8D22-45EA-B181-3EA765F92CE4} = {7F194102-A8E6-4177-8FFA-97A46D2B5991}
+		{3F83F56C-547D-4608-90A3-CD6FC1DD8D82} = {6987E055-90B9-458C-AB88-D9BD69FF10A4}
 	EndGlobalSection
 	GlobalSection(ExtensibilityGlobals) = postSolution
 		SolutionGuid = {AD304890-DEF1-462E-9016-C6FC9B72B7C6}

common/Spritify.Common.Autofac/ModuleBase.cs
@@ -1,0 +1,9 @@
+﻿using Autofac;
+
+namespace Spritify.Common.Autofac
+{
+    public abstract class ModuleBase
+    {
+        public abstract void Load(ContainerBuilder builder);
+    }
+}

common/Spritify.Common.Autofac/Spritify.Common.Autofac.csproj
@@ -1,0 +1,12 @@
+<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <TargetFramework>net5.0</TargetFramework>
+    <RestorePackagesWithLockFile>true</RestorePackagesWithLockFile>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include="Autofac" Version="6.2.0" />
+  </ItemGroup>
+
+</Project>

testFramework/Spritify.TestFramework.Extensions.Jwt/Spritify.TestFramework.Extensions.Jwt.csproj
@@ -10,7 +10,7 @@
   </ItemGroup>
 
   <ItemGroup>
-    <ProjectReference Include="..\..\common\Spritify.Autofac\Spritify.Common.Autofac.csproj" />
+    <ProjectReference Include="..\..\common\Spritify.Common.Autofac\Spritify.Common.Autofac.csproj" />
     <ProjectReference Include="..\..\common\Spritify.Common\Spritify.Common.csproj" />
     <ProjectReference Include="..\Spritify.TestFramework\Spritify.TestFramework.csproj" />
   </ItemGroup>