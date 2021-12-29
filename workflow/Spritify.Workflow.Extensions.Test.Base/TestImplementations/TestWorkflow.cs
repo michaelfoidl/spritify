88561174a81744b35b49f372a809b022e3b06e38
Michael Foidl <se18m001@technikum-wien.at>
2021-12-29 17:22:04 +0100

    Workflow - added tests for extensions

---
+++ workflow/Spritify.Workflow.Extensions.Test.Base/TestImplementations/TestWorkflow.cs
@@ -0,0 +1,13 @@
+﻿namespace Spritify.Workflow.Extensions.Test.Base.TestImplementations
+{
+    public class TestWorkflow : WorkflowBase
+    {
+        public const string StepIdentifier = "MyStep";
+
+        protected override void Configure(IWorkflowDefinitionBuilder builder)
+        {
+            builder.AddStep(step => step
+                .WithIdentifier(StepIdentifier));
+        }
+    }
+}
